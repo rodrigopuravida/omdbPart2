4558bac5e8bafb5d41849246cd41e2fd060e60ed
rodrigopuravida <rodrigo.puravida@gmail.com>
2015-07-13 08:29:41 -0700

    Adding readme and more bug fixes

--- rodrigopuravida/week2/hangman/js/script.js
+++ rodrigopuravida/week2/hangman/js/script.js
@@ -18,6 +18,9 @@ $(function(){
 
 	$('#getTip').prop('disabled', true);
 	$('#checkLetter').prop('disabled', true);
+	$('#generate-word-text').hide();
+	//for testing purposes so that I can see the word
+	//$('#generate-word-text').show();
 
 
 
@@ -27,6 +30,16 @@ $(function(){
     	word = json["word"];
     	console.log(word);
     	//console.log(wordLength);
+
+    	//tryign just to display the number of text boxes but just does not work - darn !!!!
+    // 	for (var i = word.length; i < 15; i++) {
+	   //  		 $('#letter' + i + '-text').hide();
+	 		// }
+
+	 		for (var i = 0; i < 15; i++) {
+	    		 $('#letter' + i + '-text').css('background-color' , 'yellow');
+	 		}
+
     	return word.toLowerCase();
 
     }
@@ -35,6 +48,8 @@ $(function(){
     function returnSynonims(jsonArray) {
 
     	console.log(jsonArray);
+
+    	if (jsonArray["words"]){
     	arraySynomins = jsonArray["words"];
     	//console.log(arraySynomins);
     	getTip.val(arraySynomins[tipCounter]);
@@ -64,14 +79,22 @@ $(function(){
     		for (var i = 0; i < 15; i++) {
 	    		 $('#letter' + i + '-text').css('background-color' , 'red');
 	 		}
+	 		$('#generate-word-text').show();
     		return;
     	}
+    }
   	
     }  
 	
 
-
+    //generates randomg world via calling API
   $('#generateWord-form').on('submit',function(e){
+  	winCheckCounter = 0;
+  	$('#generate-word-text').hide();
+
+  	for (var i = 0; i < 15; i++) {
+	    $('#letter' + i + '-text').css('background-color' , 'yellow');
+	 }
 
   	//location.reload();
     e.preventDefault();
@@ -91,8 +114,6 @@ $(function(){
 	      
 	 }
 
-
-
     var url = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=10000&maxCorpusCount=-1&minDictionaryCount=20&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=b68d7ebcb9910f3b8700c0b331f02428eb2fc4e009e697928';
     
     $.getJSON(url,function(wordData){
@@ -111,7 +132,7 @@ $(function(){
 	});	
 
 
-
+//fucntion that validates the letter input by user
 $('#guessWord-form').on('submit',function(e){
 	hanging = true;
 	
@@ -135,17 +156,19 @@ $('#guessWord-form').on('submit',function(e){
 
     	}
 
-
+    	//did we win?
     	console.log('counter = ' + winCheckCounter);
     	if (winCheckCounter === wordArray.length) {
     		for (var i = 0; i < 15; i++) {
 	    		 $('#letter' + i + '-text').css('background-color' , 'green');
 	 		}
+	 		$('#checkLetter').prop('disabled', true);
+	 		$('#getTip').prop('disabled', true);
     		console.log('YOU HAVE WON');
     	}
     }
 
-    //validation of correct image
+    //validation of correct image  - you have lost condition
     if (hanging) {
     	if (hangingCounter >= 8) {
     		$("#droid").attr("src","droidL" + hangingCounter + ".png");
@@ -156,6 +179,7 @@ $('#guessWord-form').on('submit',function(e){
     		for (var i = 0; i < 15; i++) {
 	    		 $('#letter' + i + '-text').css('background-color' , 'red');
 	 		}
+	 		$('#generate-word-text').show();
 
     		return;
     	}
@@ -170,7 +194,8 @@ $('#guessWord-form').on('submit',function(e){
     var guessedLetter = guessLetter.val('');
 
 });
-
+	
+	//function that generates a tip from an array
 	$('#getTip-form').on('submit',function(e){
 
 	//http://api.wordnik.com:80/v4/word.json/practice/relatedWords?useCanonical=false&limitPerRelationshipType=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
---
+++ rodrigopuravida/week2/hangman/readme.md
@@ -0,0 +1,19 @@
+A readme.md file with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+
+Pros:
+Functionality is pretty much where I wanted but needed more time.  Not because of lack of but because of being gone.
+
+Issues to work on:
+1. 2 player functionality
+2. CSS is lacking.  Wanted the game to work and then to make it pretty
+
+Approach:
+Designed the one screen game on a piece of paper as a draft and made simple html from that.  After that started coding and adding functionality.
+Used 2 APIS to get words as well as similar words for game.
+
+Unsolved problems:
+1. Better UI design. Just had no time to look into this.
+2. 2 Player mode.  Will work on this very soon.
+3. Not hosted yet.
+4. Need a way to only display text for number of characters in word.  Probably a very simple oversight.
+
